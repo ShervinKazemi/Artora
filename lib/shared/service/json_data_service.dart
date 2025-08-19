bb11cd6b8b2b01353524993360299b061ebace3c
Shervin <github.kazmy2@gmail.com>
2025-08-19 18:15:18 +0330

    feat(main): add json_data_servide.dart

---
+++ lib/shared/service/json_data_service.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:artora/core/constants.dart';
+import 'package:artora/presentation/home/data/model/nft.dart';
+import 'package:flutter/services.dart';
+
+class JsonDataService {
+  Future<List<Nft>> loadNft() async {
+    final String response = await rootBundle.loadString(AppConstants.nftJson);
+    final Map<String, dynamic> jsonData = json.decode(response);
+    final List<dynamic> nftList = jsonData["NFT"];
+    return nftList.map((nft) => Nft.fromJson(nft as Map<String, String>)).toList();
+  }
+}
