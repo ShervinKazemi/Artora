ed97eef93b2b432863ac62876b23d1f8f81c88b1
Shervin <github.kazmy2@gmail.com>
2025-08-20 00:32:37 +0330

    feat(main): add loadStats function in json_data_service.dart

--- lib/shared/service/json_data_service.dart
+++ lib/shared/service/json_data_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:artora/core/constants.dart';
 import 'package:artora/presentation/home/data/model/nft.dart';
+import 'package:artora/presentation/stats/data/model/stats.dart';
 import 'package:flutter/services.dart';
 
 class JsonDataService {
@@ -9,6 +10,17 @@ class JsonDataService {
     final String response = await rootBundle.loadString(AppConstants.nftJson);
     final Map<String, dynamic> jsonData = json.decode(response);
     final List<dynamic> nftList = jsonData["NFT"];
-    return nftList.map((nft) => Nft.fromJson(nft as Map<String, dynamic>)).toList();
+    return nftList
+        .map((nft) => Nft.fromJson(nft as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<List<Stats>> loadStats() async {
+    final String response = await rootBundle.loadString(AppConstants.statsJson);
+    final Map<String, dynamic> jsonData = json.decode(response);
+    final List<dynamic> statsList = jsonData["Stats"];
+    return statsList
+        .map((stats) => Stats.fromJson(stats as Map<String, dynamic>))
+        .toList();
   }
 }
