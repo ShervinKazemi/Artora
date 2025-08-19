845a0dd68ae3491e460617d8e17bf0c34639a1fe
Shervin <github.kazmy2@gmail.com>
2025-08-20 01:24:08 +0330

    feat(main): add activity_page.dart

--- lib/presentation/stats/widget/activity_page.dart
+++ lib/presentation/stats/widget/activity_page.dart
@@ -5,6 +5,13 @@ class ActivityPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Activity Page",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+      ),
+    );
   }
 }
